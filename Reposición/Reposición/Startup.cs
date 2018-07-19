a04e9955e1be44d866d0f49ef819990a62b74f61
ChutLuis <l.chuta2014ig@gmail.com>
2018-07-18 22:07:54 -0600

    FirstCommit

---
+++ Reposición/Reposición/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Reposición.Startup))]
+namespace Reposición
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
